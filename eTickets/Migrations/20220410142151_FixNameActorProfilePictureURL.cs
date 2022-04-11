7ad4247b5bc5f2294a27797f55cde49141b13aed
Edalat Feizi <edalatfeizi921@gmail.com>
2022-04-11 14:50:22 +0430

    entity models, db context , db initializer and also database migrations added.

---
+++ eTickets/Migrations/20220410142151_FixNameActorProfilePictureURL.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace eTickets.Migrations
+{
+    public partial class FixNameActorProfilePictureURL : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ProfilePictureUrl",
+                table: "Actors",
+                newName: "ProfilePictureURL");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ProfilePictureURL",
+                table: "Actors",
+                newName: "ProfilePictureUrl");
+        }
+    }
+}
--- eTickets/Program.cs
+++ eTickets/Program.cs
@@ -1,7 +1,16 @@
+using eTickets.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+//DbContext Configuration
+builder.Services.AddDbContext<AppDbContext>(options => 
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString"));
+});
 
 var app = builder.Build();
 
@@ -23,5 +32,8 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
+AppDbInitializer.Seed(app);
 
 app.Run();
